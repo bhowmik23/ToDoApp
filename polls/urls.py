0ca6d52368f525c16c26eb4a6e5910eb0e39ee21
Biddut Bhowmik <54990169+bhowmik23@users.noreply.github.com>
2020-09-20 19:24:36 +0600

    Done my todo app

--- polls/urls.py
+++ polls/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('add_todo/', views.todo, name="todo")
+    path('add_todo/', views.todo, name="todo"),
+    path('delete_todo/<int:todo_id>', views.delete_todo, name="delete_todo")
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.utils import timezone
 from polls.models import Todo
-
+from django.http import HttpResponseRedirect
 
 def home(request):
-    return render(request, 'main/index.html')
+    todo_items = Todo.objects.all().order_by('added_date')
+    return render(request, 'main/index.html', {"todo_items":todo_items})
  
 def todo(request):
     # print(request.POST.get('todo'))
@@ -13,4 +14,9 @@ def todo(request):
     # print(added_date)
     # print(content)
     created_obj =  Todo.objects.create(added_date= current_date, text= content)
-    return render(request, 'main/index.html')
+    length_of_todos = Todo.objects.all().count()
+    # return render(request, 'main/index.html')
+    return HttpResponseRedirect("/")
+def delete_todo(request, todo_id):
+    Todo.objects.get(id= todo_id).delete()
+    return HttpResponseRedirect("/")
