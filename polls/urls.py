04e66f782f6dcc12dc0c4c7aa516556598b7512a
Biddut Bhowmik <54990169+bhowmik23@users.noreply.github.com>
2020-09-20 18:47:05 +0600

    create todo function to insert database

--- polls/urls.py
+++ polls/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('add_todo/', views.todo, name="todo")
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,16 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from django.utils import timezone
+from polls.models import Todo
 
 
-# def index(request):
-#     return HttpResponse("Hello, world. You're at the polls index.")
 def home(request):
-    return render(request, 'base.html')
+    return render(request, 'main/index.html')
+ 
+def todo(request):
+    # print(request.POST.get('todo'))
+    current_date = timezone.now()
+    content = request.POST["todo"]
+    # print(added_date)
+    # print(content)
+    created_obj =  Todo.objects.create(added_date= current_date, text= content)
+    return render(request, 'main/index.html')
